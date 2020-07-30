09b0d92fca0d44551639fe67e3ed393c73d8c28d
Sean Ransonette <sransonette@gmail.com>
2020-07-30 03:59:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,15 @@
 class Book
+<<<<<<< HEAD
   attr_accessor :author, :page_count, :genre
   attr_reader :title
   
+=======
+>>>>>>> 1a370dc3a1f7c12b1e7d9d2edc25a6cdc2ae7a69
   def initialize(title)
     @title = title
   end
   
+<<<<<<< HEAD
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
@@ -19,5 +23,26 @@ end
 
 
 
+=======
+  def title
+    @title
+  end
+>>>>>>> 1a370dc3a1f7c12b1e7d9d2edc25a6cdc2ae7a69
 
+  def author=(author)
+    @author = author 
+  end
+  
+  
+
+
+
+
+
+
+
+
+
+
+end
 
